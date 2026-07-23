d35bd643cde5c3aa6efa8a3cb542ec0e895ef807
oticahorizonte <mayconaraujo2611@gmail.com>
2026-07-22 22:53:34 -0400

    SW limpo para /app/

---
+++ app/sw.js
@@ -0,0 +1,7 @@
+// SW vazio - sem cache
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
+  .then(() => self.clients.claim())
+));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
